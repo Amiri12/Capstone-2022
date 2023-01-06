2899ce23bcc7efa8098eaa4a72ac58b08bbf8f1f
Amiri12 <44327702+Amiri12@users.noreply.github.com>
2023-01-06 12:24:29 -0800

    individual control of motors for calibration

--- Arm/Arm.ino
+++ Arm/Arm.ino
@@ -6,14 +6,181 @@ int in1;
 int in2;
 int in3;
 int in4;
+int nib;
 void setup() {
   
   Serial.begin(9600);
 }
 
 void loop() {
-    in1 = analogRead(A2);
-    in2 = analogRead(A3);
-    in3 = analogRead(A4);
+    in1 = analogRead(A2)*1000;
+    in2 = analogRead(A3)*100;
+    in3 = analogRead(A4)*10;
     in4 = analogRead(A5);
+    nib = in1+in2+in3+in4;
+    Serial.println(nib);
+    if(nib == 0001){
+      M1.start(1);
+      M2.start(-1);
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(nib == 0010){
+      M1.stop();
+      M2.stop();
+      M3.start(1);
+      M4.start(-1);
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(nib == 0011){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.start(1);
+      M6.start(-1);
+      M7.stop();
+      M8.stop();
+    }else if(nib == 0100){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.start(1);
+      M8.start(-1);      
+    }else if(nib == 0101){
+      M1.start(-1);
+      M2.start(1);
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(nib == 0110){
+      M1.stop();
+      M2.stop();
+      M3.start(-1);
+      M4.start(1);
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();      
+    }else if(nib == 0111){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.start(-1);
+      M6.start(1);
+      M7.stop();
+      M8.stop();
+    }else if(nib == 1000){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.start(-1);
+      M8.start(1);
+    }else if(nib == 0000){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 1){
+      M1.start(in4);
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 2){
+      M1.stop();
+      M2.start(in4));
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 3){
+      M1.stop();
+      M2.stop();
+      M3.start(in4));
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 4){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.start(in4));
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 5){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.start(in4));
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 6){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.start(in4));
+      M7.stop();
+      M8.stop();
+    }else if(in1/1000 == 7){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.start(in4));
+      M8.stop();
+    }else if(in1/1000 == 8){
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.start(in4));      
+    }else{
+      M1.stop();
+      M2.stop();
+      M3.stop();
+      M4.stop();
+      M5.stop();
+      M6.stop();
+      M7.stop();
+      M8.stop();
+    }
 }
+
---
+++ controller/controller_calibration/controller_calibration.ino
@@ -0,0 +1,85 @@
+int LX = 0;
+int LY = 0;
+int RX = 0;
+int RY = 0;
+int LXO = 0;
+int LYO = 0;
+int RXO = 0;
+int RYO = 0;  // value read from the pot
+int out[] = {0,0,0,0};
+  // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // read the analog in value:
+  RX = analogRead(A0);
+  RY = analogRead(A1);
+  LX = analogRead(A3);
+  LY = analogRead(A2);
+  // map it to the range of the analog out:
+  RXO = map(RX, 0, 1023, -512, 512);
+  LXO = map(LX, 0, 1023, -512, 512);
+  RYO = map(RY, 0, 1023, -512, 512);
+  LYO = map(LY, 0, 1023, -512, 512);
+  // change the analog out value:
+  //analogWrite(analogOutPin, outputValue);
+
+  // print the results to the Serial Monitor:
+  Serial.print("RX=");
+  Serial.print(RXO);
+  Serial.print("\t RY=");
+  Serial.print(RYO);
+  Serial.print("\t LX=");
+  Serial.print(LXO);
+  Serial.print("\t LY=");
+  Serial.print(LYO);
+  Serial.print("\t OUT=");
+  Serial.print(out[0]);
+  Serial.print(out[1]);
+  Serial.print(out[2]);
+  Serial.println(out[3]);
+  
+  if(RYO > 250 && -240 < RXO && RXO < 240){
+    out[3] = 1;
+  }
+  else if(RYO < -250 && -240 < RXO && RXO < 240){
+    out[3] = 5;
+  }
+  else if(RXO < -250 && -240 < RYO && RYO < 240){
+    out[3] = 7;
+  }
+  else if(RXO > 250 && -240 < RYO && RYO < 240){
+    out[3] = 3;
+  }
+  else if(RXO > 250 && RYO > 250){
+    out[3] = 2;
+  }
+  else if(RXO < -250 && RYO < -250){
+    out[3] = 6;
+  }
+  else if(RXO < -250 && RYO > 250){
+    out[3] = 8;
+  } 
+  else if(RXO > 250 && RYO < -250){
+    out[3] = 4;
+  }
+  else{
+    out[3] = 0;
+  }
+
+  if(LYO > 150){
+    out[0] = 1;
+  }else if(LYO < -150){
+    out[0] = -1;
+  }else{
+    out[0] = 0;
+  }
+}
